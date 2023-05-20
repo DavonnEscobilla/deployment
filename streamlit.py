8aaa6ebaa92bdc39301dde7e0e4b81405c421dd3
DavonnEscobilla <112688457+DavonnEscobilla@users.noreply.github.com>
2023-05-20 18:47:40 +0800

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('final_model.h5')
+  return model
+model=load_model()
+st.write("""
+# Fashion MNIST"""
+)
+file=st.file_uploader("Choose fashion item photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['0: T-shirt/top','1: Trouser',
+                 '2: Pullover',
+                 '3: Dress',
+                 '4: Coat',
+                 '5: Sandal',
+                 '6: Shirt',
+                 '7: Sneaker',
+                 '8: Bag',
+                 '9: Ankle boot']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
