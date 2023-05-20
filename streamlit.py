422ceba42672b61192eb820557563036c7f87a85
DavonnEscobilla <112688457+DavonnEscobilla@users.noreply.github.com>
2023-05-20 19:27:44 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,27 +14,28 @@ file=st.file_uploader("Choose fashion item photo from computer",type=["jpg","png
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+def import_and_predict(image_data, model):
+    # Preprocess the image
+    image = image_data.convert('L')
+    image = image.resize((28, 28))
+    image = np.array(image)
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    # Make predictions
+    prediction = model.predict(image)
+    class_names = [
+        'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+    ]
+    predicted_class = np.argmax(prediction)
+    output = f"Prediction: {class_names[predicted_class]}"
+    return output
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['0: T-shirt/top','1: Trouser',
-                 '2: Pullover',
-                 '3: Dress',
-                 '4: Coat',
-                 '5: Sandal',
-                 '6: Shirt',
-                 '7: Sneaker',
-                 '8: Bag',
-                 '9: Ankle boot']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    output = import_and_predict(image, model)
+    st.success(output)
