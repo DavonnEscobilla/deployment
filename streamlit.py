b0923582ec71f2aff8eb0e594c66549362ac9e1d
DavonnEscobilla <112688457+DavonnEscobilla@users.noreply.github.com>
2023-05-20 19:12:11 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,38 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('final_model.h5')
-  return model
-model=load_model()
-st.write("""
-# Fashion MNIST"""
-)
-file=st.file_uploader("Choose fashion item photo from computer",type=["jpg","png"])
+    model = tf.keras.models.load_model('final_model.h5')
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['0: T-shirt/top','1: Trouser',
-                 '2: Pullover',
-                 '3: Dress',
-                 '4: Coat',
-                 '5: Sandal',
-                 '6: Shirt',
-                 '7: Sneaker',
-                 '8: Bag',
-                 '9: Ankle boot']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+
+def main():
+    st.write("# Fashion MNIST")
+
+    model = load_model()
+
+    file = st.file_uploader("Choose a fashion item photo from your computer", type=["jpg", "png"])
+
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+
+        class_names = ['0: T-shirt/top', '1: Trouser', '2: Pullover', '3: Dress', '4: Coat', '5: Sandal', '6: Shirt', '7: Sneaker', '8: Bag', '9: Ankle boot']
+        output_string = "OUTPUT: " + class_names[np.argmax(prediction)]
+        st.success(output_string)
+
+if __name__ == '__main__':
+    main()
